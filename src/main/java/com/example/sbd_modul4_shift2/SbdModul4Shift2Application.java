fdc81fd407e52be619c5eb77b764de70a29ac210
unknown <muhammadirhamsyah26@gmail.com>
2023-10-08 08:30:33 +0700

    first commit

---
+++ src/main/java/com/example/sbd_modul4_shift2/SbdModul4Shift2Application.java
@@ -0,0 +1,13 @@
+package com.example.sbd_modul4_shift2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdModul4Shift2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdModul4Shift2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbd_modul4_shift2/SbdModul4Shift2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbd_modul4_shift2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdModul4Shift2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
